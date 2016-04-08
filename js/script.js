759aadf5cf5758c6ce153c4a6d293015c68164e6
Thiago Nascimento <thiagodnf@gmail.com>
2016-04-08 14:04:47 -0300

    adicionado modal com as proximas estacoes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 var season_now = null;
 
+var summer = [];
+var spring = [];
+var fall = [];
+var winter = [];
+var next_seasons = [];
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 function resizeWindow(){
 
 	moment.locale("pt-br");
@@ -57,11 +67,79 @@ $(function(){
 						min_diff = diff;
 						season_now = val;
 					}
+				}else{
+					next_seasons.push(val);
+				}
+
+
+				if(val.season_name == "Outono"){
+					fall.push(val);
+				}else if(val.season_name == "Verão"){
+					summer.push(val);
+				}else if(val.season_name == "Primavera"){
+					spring.push(val);
+				}else if(val.season_name == "Inverno"){
+					winter.push(val);
 				}
 			});
 
+			// Order the seasons by date
+			next_seasons.sort(function(a, b) {
+			    a = new Date(a.start_date);
+			    b = new Date(b.start_date);
+			    return a>b ? 1 : a<b ? -1 : 0;
+			});
+
+			moment.locale("pt-br");
+
+			var str = "";
+
+			for(var i =0;i<4;i++){
+				var date = moment(next_seasons[i].start_date);
+
+
+				str += "<div class='row'>";
+				//$("#the-next-seasons").append("<div class='col-xs-2 nopadding'>");
+
+				if(next_seasons[i].season_name == "Outono"){
+					str +="<div class='col-xs-2 nopadding'><img class='img-responsive' src='img/f_fall.png'></div>"
+				}else if(next_seasons[i].season_name == "Verão"){
+					str +="<div class='col-xs-2 nopadding'><img class='img-responsive' src='img/f_summer.png'></div>"
+				}else if(next_seasons[i].season_name == "Primavera"){
+					str +="<div class='col-xs-2 nopadding'><img class='img-responsive' src='img/f_spring.png'></div>"
+				}else if(next_seasons[i].season_name == "Inverno"){
+					str +="<div class='col-xs-2 nopadding'><img class='img-responsive' src='img/f_winter.png'></div>"
+				}
+
+				//$("#the-next-seasons").append("</div>");
+
+				str +="<div class='col-xs-10 nopadding'>";
+				str +="<p class='bold'>"+next_seasons[i].season_name+"</p>";
+				str +="<p> Começa "+date.fromNow()+" na "+date.format('LLLL')+"</p>";
+				str +="</div>";
+				str +="</div><br>";
+			}
+
+			$("#the-next-seasons").append(str);
+
+
+
+
+			// $.each( next_seasons, function( key, val ) {
+			// 	$("#the-next-seasons").append("<li>Em "+moment(val.start_date).fromNow()+" começa o "+val.season_name+"</li>")
+			// });
+
 			resizeWindow();
 		}
+	});
+
+	$('#modal-see-more').on('show.bs.modal', function () {
+		$('.modal .modal-body').css('overflow-y', 'auto');
+    	$('.modal .modal-body').css('max-height', $(window).height() * 0.6);
+	});
+
+	$("#btn-see-more").click(function(){
+		$("#modal-see-more").modal('show');
 	});
 
 	$(window).resize(function(){
